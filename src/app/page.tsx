bd4e51c111fa80258e0db4a1f7bb115cb17b6f6a
rajatjain22 <jainr6000@gmail.com>
2024-01-17 01:32:18 +0530

    First push

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+import HomeContainer from '@/components/Home/HomeContainer/HomeContainer';
+import Image from 'next/image';
+
+export default function Home() {
+	return <HomeContainer />;
+}
---
+++ src/components/Home/HomeContainer/HomeContainer.tsx
@@ -0,0 +1,10 @@
+import SideNavContainer from '@/components/SideNav/SideNavContainer';
+
+export default function HomeContainer() {
+	return (
+		<div className='flex min-h-screen w-full'>
+			<SideNavContainer />
+            <div className='w-4/5'>wjahgh</div>
+		</div>
+	);
+}
